3bd01b9b27df8b2b13868424501a6284de54b391
zligusi <tort24abcd@gmaill.com>
2026-05-12 20:31:03 +0200

    I have problem...

--- kalkylator.py
+++ kalkylator.py
@@ -1,83 +1,43 @@
 # The cod is a mini kalkylator 
+while go == "exit" :        
+    while True :           # проверка на число
+        try :                
+            a = int(input("Введите первое число : "))
+            break
+        except ValueError :
+            print("Вы ввели не число ")
 
-while True :           # проверка на число
-    try :                
-        a = int(input("Введите первое число : "))
-        break
-    except ValueError :
-        print("Вы ввели не число ")
-
-while True :        # цикл  / знак операции
-    want = input("Что делаем ? (+ , - ,  * ,  : )")
-    if want in ["+" , "-" , "*" , ":"] :
-        break
-    else : 
-        print("Вы ввели не правильный знак операции ")
+    while True :        # цикл  / знак операции
+        want = input("Что делаем ? (+ , - ,  * ,  : )")
+        if want in ["+" , "-" , "*" , ":"] :
+            break
+        else : 
+            print("Вы ввели не правильный знак операции ")
         
-while True :        #проверка на число
-    try :
-        b = int(input("Введите второе число : "))
-        break
-    except ValueError :
-        print("Вы ввели не число ")
-
-
-
-if want == "+" :
-    c = a + b 
-elif want == "-" :
-    c = a - b 
-elif want == "*" :
-    c = a * b
-elif want == ":" :
-    c = a / b
-else :
-    print("Вы ввели не правильный знак операции ")
-
-
-print(f"Результат : {c}")
-
-
-
-while True :
-    go = input(str("Хотите продолжить ? (yes / no) : ")).strip().lower()
-    
-    if go in ["yes" , "no"]:
-        break 
-
-if go == "yes" :
-    save = input(str("Использовать предыдущий результат ? (yes / no) : ")).strip().lower()
-elif go == "no" :
-    print("До свидания !")
-    exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-p = 0
+    while True :        #проверка на число
+        try :
+            b = int(input("Введите второе число : "))
+            break
+        except ValueError :
+            print("Вы ввели не число ")
+    if want == "+" :
+        c = a + b 
+    elif want == "-" :
+        c = a - b 
+    elif want == "*" :
+        c = a * b
+    elif want == ":" :
+        c = a / b
+    else :
+        print("Вы ввели не правильный знак операции ")
+    print(f"Результат : {c}")
+    while True :
+        go = input(str("Хотите продолжить ? (yes / exit) : ")).strip().lower()
+        if go in ["yes" , "exit"]:
+            break 
+        if go == "yes" :
+            save = input(str("Использовать предыдущий результат ? (yes / no) : ")).strip().lower()
+            
+        if go == "exit" : 
+            print("До свидания !")
+            break
